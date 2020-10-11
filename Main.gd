f70d070e052fb5a65d33ae7ab213258a25b4e90e
Anton Borodko <antonborodko@gmail.com>
2020-10-11 20:01:17 +0300

    Added turn indicator updated by Enter

--- Main.gd
+++ Main.gd
@@ -5,16 +5,26 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
+var turn = 0
+onready var cheatMenu = get_node("CheatPanel") as Panel
+onready var turnLabel = get_node("TurnLabel") as RichTextLabel
+
+func updateTurn(t = turn):
+	turnLabel.bbcode_text = "[center]Turn %s[/center]" % (t +1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	updateTurn()
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		turn +=1
+		updateTurn()
+		
 	if Input.is_action_just_pressed("ui_show_cheat_menu"):
-		var cheatMenu = get_node("CheatPanel") as Panel
 		if cheatMenu.visible:
 			cheatMenu.hide()
 		else:
