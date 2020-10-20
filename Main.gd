2ad9825f1dfe5d24e374018aad6b0a55e17cbe75
Anton Borodko <antonborodko@gmail.com>
2020-10-20 20:07:10 +0300

    Working on house placement

--- Main.gd
+++ Main.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var houseScene = preload("res://scenes/settlements/House2D.tscn")
+
 var turn = 0
 var totalPopulation=0
 
@@ -17,6 +19,22 @@ const AREAWIDTH = 80
 class Road:
 	var from:Vector2
 	var to: Vector2
+	
+	func getTiles() -> PoolVector2Array:
+		var to_x = to.x
+		if (to_x == from.x):
+			to_x += 1
+		
+		var to_y = to.y
+		if (to_y == from.y):
+			to_y +=1
+		
+		var response = PoolVector2Array()
+		for x in range(from.x,to_x):
+			for y in range(from.y,to_y):
+				response.append(Vector2(x,y))
+				
+		return response
 	
 	func isVertical()->bool:
 		return (from.x == to.x)
@@ -50,39 +68,47 @@ func accomodatePopulation(pop:int,totalPopulation:int,roads:Array,houses:Array)-
 	placeHouses(newHousesNeeded,roads,houses)
 	return AccomodatePopulationResponse.new(newTotalPop,roads,houses)
 
-func _getFreeBlocks(road:Road)->PoolVector2Array:
-	return PoolVector2Array()
+func _getFreeBlocks(road:Road, houses:Array)->PoolVector2Array:
+	var tiles = road.getTiles()
+	var response = PoolVector2Array()
+	for tile in tiles:
+		if road.isVertical():
+			response.append(Vector2(tile.x-1,tile.y))
+			response.append(Vector2(tile.x+1, tile.y))
+		else:
+			response.append(Vector2(tile.x,tile.y-1))
+			response.append(Vector2(tile.x,tile.y+1))
+	return response
 
 func placeHouses(newHouses:int,roads:Array,houses:Array): #returns array of vectors
 	for road in roads:
-		var freeBlocks = _getFreeBlocks(road)
+		var freeBlocks = _getFreeBlocks(road,houses)
+		for block in freeBlocks:
+			print("%s/%s" % [block.x, block.y])
 	
 func updateTurn(t = turn):
 	accomodatePopulation(POPDELTA, totalPopulation, roads, houses)
 	drawRoads(roads)
+	drawHouses(houses)
 	turnLabel.bbcode_text = "[center]Turn %s[/center]" % (t +1)
 
 func drawRoads(roads: Array):
 	for road in roads:
-		var to_x = road.to.x
-		if (to_x == road.from.x):
-			to_x +=1
+		for tile in road.getTiles():
+			desertTilemap.set_cell(tile.x,tile.y,1)
+
+func drawHouses(houses: Array):
+	for house in houses:
+		var node = houseScene.instance() as Node2D
+		node.set_position(Vector2(house.topLeftCoords.x,house.topLeftCoords.y))
+		desertTilemap.add_child(node)
 		
-		var to_y = road.to.y
-		if (to_y == road.from.y):
-			to_y +=1
-		
-		for x in range(road.from.x,to_x):
-			for y in range(road.from.y,to_y):
-				print(x)
-				print(y)
-				desertTilemap.set_cell(x,y,1)
-	pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	roads.append(Road.new(Vector2(0,16),Vector2(32,16)))
-	roads.append(Road.new(Vector2(32,16),Vector2(32,24)))
+	houses.append(House.new(Vector2(10,14)))
+#	roads.append(Road.new(Vector2(32,16),Vector2(32,24)))
 	updateTurn()
 
 
