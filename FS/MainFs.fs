72f0b1a939ea5cc4dd5b5864248b6105e5a9b9da
Anton Borodko <antonborodko@gmail.com>
2020-10-24 20:07:04 +0300

    Added cheat menu

--- FS/MainFs.fs
+++ FS/MainFs.fs
@@ -6,17 +6,23 @@ type MainFs() as this =
     inherit Node()
     
     let turnLabel = lazy(this.GetNode(new NodePath("TurnLabel")) :?> RichTextLabel)
+    let cheatMenu = lazy(this.GetNode(new NodePath("CheatPanel")) :?> Panel)
     let mutable turn = 0
     let updateTurn turn =
         let newTurn = turn+1
-        GD.Print(newTurn)
-        turnLabel.Value.BbcodeText <- sprintf "[center]Turn %d [/center]" newTurn
         newTurn
     
     override this._Ready() =
         turn <- updateTurn turn
 
-    override this._Process(delta) =
+    override this._Process(_) =
         turn <- if Input.IsActionJustPressed("ui_accept") then updateTurn turn else turn
-        ()
+        turnLabel.Value.BbcodeText <- sprintf "[center]Turn %d [/center]" turn
+        
+        if Input.IsActionJustPressed("ui_show_cheat_menu") then
+            if (cheatMenu.Value.Visible) then
+                cheatMenu.Value.Hide()
+            else
+                cheatMenu.Value.Show()
+
 
