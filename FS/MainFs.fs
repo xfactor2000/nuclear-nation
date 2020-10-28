f21125c6c687a5a5278bdba9cc2bf9d108ab6645
Anton Borodko <antonborodko@gmail.com>
2020-10-28 21:38:45 +0300

    Restored let bindings style

--- FS/MainFs.fs
+++ FS/MainFs.fs
@@ -18,25 +18,26 @@ type MainFs() as this =
     
     let mutable totalPopulation = 0
     
-    member private this.UpdateTurn(turn) =
+    let updateTurn turn =
         turn + 1
     
-    member private this.DrawRoads() =
+    let drawRoads() =
         let roads = this.GetChildren().Cast<Node>().Where(fun (n)-> n :? Road).Cast<Road>().ToList()
         for road in roads do
             for tile in road.GetTiles() do
                 desertTilemap.Value.SetCell(int tile.x, int tile.y, 1)
+    
     
     override this._Ready() =
         let house = houseScene.Instance() :?> HouseFs;
         this.AddChild(house);
         house.Position <- Vector2(float32 2 * desertTilemap.Value.CellSize.x,float32 15*desertTilemap.Value.CellSize.y);
         this.AddChild(new Road(Vector2(float32 0, float32 16), Vector2(float32 8, float32 16)))
-        this.DrawRoads()
-        turn <- this.UpdateTurn(turn)
+        drawRoads()
+        turn <- updateTurn turn
 
     override this._Process(_) =
-        turn <- if Input.IsActionJustPressed("ui_accept") then this.UpdateTurn(turn) else turn
+        turn <- if Input.IsActionJustPressed("ui_accept") then updateTurn turn else turn
         turnLabel.Value.BbcodeText <- sprintf "[center]Turn %d [/center]" turn
         
         if Input.IsActionJustPressed("ui_show_cheat_menu") then
