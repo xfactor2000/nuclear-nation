aaacef13b83efcc502aeebc75bfa3257af811eed
Anton <antonborodko@gmail.com>
2020-12-26 22:07:17 +0100

    Working on adding children to entities

--- core/src/com/anton/nuclearnation/map/package.scala
+++ core/src/com/anton/nuclearnation/map/package.scala
@@ -5,4 +5,11 @@ package object map {
    * Defines the size of the basic map tile
    */
   val (mapTileSizeX:Int,mapTileSizeY:Int) = (32,32)
+
+  /**
+   * Defines the map data for this specific map
+   */
+  val mapWidthTiles = 60
+  val mapHeightTiles = 60
+  val mapData = new MapData(mapWidthTiles,mapHeightTiles)
 }
